880126c6059669a2b79fdc20a54a570507f0d0f9
PranavGarg92 <gargpranav9292@gmail.com>
2025-03-18 18:59:12 +0530

    css added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,12 +3,14 @@ import ResCard from "./ResCard";
 function Body(){
     return(
         <div className="body">
-            <input  className="search" placeholder="search"></input>
+            <input  className="search" placeholder="search..."></input>
             <div className="res-container">
                 <ResCard resName="McD" cuisine="Burgers, Fries, Coke"/>
                 <ResCard resName="Burger King" cuisine="Burgers, Fries, Coke"/>
                 <ResCard resName="Sagar Ratna" cuisine="Dosa, Idli ,Sambar, Chatni, Chatni"/>
                 <ResCard resName="Bhojnam" cuisine="Thali, Rice, North Indian"/>
+                <ResCard resName="McD" cuisine="Burgers, Fries, Coke"/>
+                <ResCard resName="McD" cuisine="Burgers, Fries, Coke"/>
             </div>
         </div>
     )
